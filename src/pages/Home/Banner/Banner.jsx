aba92113ebf716a778ff7a62857f9062486b4fe4
Sultan Mahmud <mdsultanmahmud.bd00@gmail.com>
2022-12-10 19:33:44 +0600

    add typing effect

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -1,7 +1,7 @@
 
 import React from 'react';
-import banner from '../../../assets/banner.png'
 import banner1 from '../../../assets/banner1.png'
+import Typed from 'react-typed'
 const Banner = () => {
     const onButtonClick = () => {
         // using Java Script method to get PDF file
@@ -16,16 +16,28 @@ const Banner = () => {
         })
     }
     return (
-            <div className="hero md:h-[600px] bg-black">
-                <div className="hero-content flex-col lg:flex-row-reverse">
-                    <img src={banner1} className='bg-black mb-5 h-full rounded-lg shadow-2xl'/>
-                    <div>
-                        <h1 className="text-5xl font-bold text-white">Hi, I am Sultan Mahmud</h1>
-                        <p className="py-3 text-3xl font-semibold text-red-600">A Junior Web Developer</p>
-                        <button onClick={onButtonClick} className="btn btn-secondary btn-outline">Download Resume</button>
-                    </div>
+        <div className="hero md:h-[600px] bg-black">
+            <div className="hero-content flex-col lg:flex-row-reverse">
+                <img src={banner1} className='bg-black mb-5 h-full rounded-lg shadow-2xl' />
+                <div>
+                    <h1 className="text-5xl font-bold text-white">Hi, I am Sultan Mahmud</h1>
+                    <p className="py-3 text-3xl font-semibold text-red-600">
+                    <Typed
+                            strings={[
+                                "I'm a Junior Web Developer",
+                                "I'm a Junior React Developer",
+                                "I'm a Junior MERN Developer",
+                            ]}
+                            typeSpeed={150}
+                            backSpeed={150}
+                            loop
+                           />
+                    </p>
+
+                    <button onClick={onButtonClick} className="btn btn-secondary btn-outline">Download Resume</button>
                 </div>
             </div>
+        </div>
     );
 };
 
